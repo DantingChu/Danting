2d155e0ab0422dc9699f295e60ce7cd5bcb215a2
DantingChu <63572771+DantingChu@users.noreply.github.com>
2020-04-26 14:41:25 +0000

    Updated Project 4
    ListView & SectionList

--- MyFourthProject/App copy 2.js
+++ MyFourthProject/App copy 2.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
 
-export default function App() {
+export default class MyApp extends Component {
+  render(){
+
   return (
-    <View style={styles.container}>
+    <View style={{padding: 60}}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>I like using the class component!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
